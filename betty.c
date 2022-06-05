d99cf9652c702c6748ae7ca683e70cc05ba6cf57
fiansky <samuel.asare@azubiafrica.org>
2022-06-05 15:27:43 +0000

    make betty proud

---
+++ betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- the main function
+ *my hello. printf task
+ * Return: 0 after printing your function
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
